aab1484151393a539525398d2c9e41a4837cab8e
sehandev <sehandev@gmail.com>
2022-01-01 18:32:17 +0900

    EDIT attendance: user_info

--- components/attendance.jsx
+++ components/attendance.jsx
@@ -8,6 +8,7 @@ import {
   DescriptionStrong,
   Checkbox,
 } from './common'
+import { useEffect, useState } from 'react'
 
 const Column = styled.th`
   width: 80px;
@@ -20,6 +21,62 @@ const LongColumn = styled.th`
 `
 
 const Attendance = () => {
+  const [count, set_count] = useState(0)
+  const [user_info_array, set_user_info_array] = useState([])
+
+  useEffect(() => {
+    set_user_info_array([
+      {
+        id: '1234',
+        name: '이름순',
+        sex: '남',
+        check_1: false,
+      },
+      {
+        id: '2307',
+        name: '김다연',
+        sex: '여',
+        check_1: true,
+      },
+      {
+        id: '2307',
+        name: '김성돈',
+        sex: '남',
+        check_1: false,
+      },
+      {
+        id: '2584',
+        name: '박세연',
+        sex: '여',
+        check_1: false,
+      },
+      {
+        id: '9018',
+        name: '복영빈',
+        sex: '여',
+        check_1: true,
+      },
+      {
+        id: '6195',
+        name: '이도연',
+        sex: '여',
+        check_1: true,
+      },
+    ])
+  }, [])
+
+  useEffect(() => {}, [count])
+
+  const check_attendance = (index) => {
+    set_user_info_array((prev_array) => {
+      console.log(prev_array[index])
+      prev_array[index].check_1 = !prev_array[index].check_1
+      console.log(prev_array[index])
+      return prev_array
+    })
+    set_count((prev_state) => prev_state + 1)
+  }
+
   const Table = () => (
     <table className='mt-8 table-fixed border-collapse border'>
       <thead className='bg-gray-50'>
@@ -39,30 +96,20 @@ const Attendance = () => {
         </tr>
       </thead>
       <tbody className='text-center'>
-        <tr className='h-20'>
-          <td className='border'>1234</td>
-          <td className='border'>이름순</td>
-          <td className='border'>남</td>
-          <td className='border'>
-            <Checkbox check />
-          </td>
-        </tr>
-        <tr className='h-20'>
-          <td className='border'>1234</td>
-          <td className='border'>이름순</td>
-          <td className='border'>남</td>
-          <td className='border'>
-            <Checkbox />
-          </td>
-        </tr>
-        <tr className='h-20'>
-          <td className='border'>1234</td>
-          <td className='border'>이름순</td>
-          <td className='border'>남</td>
-          <td className='border'>
-            <Checkbox check />
-          </td>
-        </tr>
+        {user_info_array.map((user_info, index) => (
+          <tr
+            key={index}
+            className='h-20 hover:bg-violet-50 cursor-pointer'
+            onClick={() => check_attendance(index)}
+          >
+            <td className='border'>{user_info.id}</td>
+            <td className='border'>{user_info.name}</td>
+            <td className='border'>{user_info.sex}</td>
+            <td className='border'>
+              <Checkbox check={user_info.check_1} />
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   )
