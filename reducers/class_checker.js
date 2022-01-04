cda6e134223bbfd79c90176cc8bfcb677856a185
sehandev <sehandev@gmail.com>
2022-01-04 17:23:06 +0900

    EDIT class_checker: init class

--- reducers/class_checker.js
+++ reducers/class_checker.js
@@ -1,14 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const class_check_slice = createSlice({
-  name: 'class_check',
-  initialState: { grade: 1, class: 1 },
+const class_checker_slice = createSlice({
+  name: 'class_checker',
+  initialState: { grade: -1, class: -1 },
   reducers: {
+    initialize_class(state) {
+      state = Object.assign(state, { grade: -1, class: -1 })
+    },
     change_class(state, action) {
       state = Object.assign(state, action.payload)
     },
   },
 })
 
-export const { change_class } = class_check_slice.actions
-export default class_check_slice.reducer
+export const { initialize_class, change_class } = class_checker_slice.actions
+export default class_checker_slice.reducer
