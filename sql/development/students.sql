1777efd33e139e7d654fb160b20353a6539ccb4b
DoyeonLee <dyl1229@naver.com>
2022-04-16 23:21:26 +0900

    initial commit

---
+++ sql/development/students.sql
@@ -0,0 +1,10 @@
+/*테이블 create*/
+CREATE TABLE STUDENTS(
+    student_id NCHAR(8) NOT NULL,
+    student_name NCHAR(10) NOT NULL,
+    sex CHAR(1) NOT NULL,
+    student_state CHAR(1) NOT NULL,
+    CONSTRAINT students_pk PRIMARY KEY (student_id)
+);
+
+/*테이블 전체 삭제*/
