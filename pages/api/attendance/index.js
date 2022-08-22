593884957bed8df1a2bcb4a04fd3bc4575504f89
sehandev <sehandev@gmail.com>
2022-08-22 16:33:50 +0000

    [EDIT] Replace attendance by date

--- pages/api/attendance/index.js
+++ pages/api/attendance/index.js
@@ -1,22 +1,28 @@
 import db from '../../../utils/firestore'
 
 export default async (req, res) => {
-  if (req.method !== 'POST') {
-    res.status(400).end()
-  }
   try {
     const attendance = await db
       .collection('attendance')
       .where('date', '==', req.body.date)
       .where('member_id', '==', req.body.member_id)
       .get()
-    if (0 < attendance.docs.length) {
-      res.status(404).end()
-    } else {
-      const { id } = await db.collection('attendance').add({
-        ...req.body,
-      })
-      res.status(200).json({ id })
+    if (req.method == 'POST') {
+      if (0 < attendance.docs.length) {
+        res.status(404).end()
+      } else {
+        const { id } = await db.collection('attendance').add({
+          ...req.body,
+        })
+        res.status(200).json({ id })
+      }
+    } else if (req.method == 'DELETE') {
+      if (attendance.docs.length === 0) {
+        res.status(404).end()
+      } else {
+        await db.collection('attendance').doc(attendance.docs[0].id).delete()
+        res.status(200).end()
+      }
     }
   } catch (e) {
     console.error(e)
