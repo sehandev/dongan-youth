350e6ed76b759db87667502143a20bef0e17e83c
sehandev <sehandev@gmail.com>
2022-08-22 16:07:49 +0000

    [EDIT] Replace attendance create

---
+++ pages/api/attendance/index.js
@@ -0,0 +1,22 @@
+import db from '../../../utils/firestore'
+
+export default async (req, res) => {
+  try {
+    const attendance = await db
+      .collection('attendance')
+      .where('date', '==', req.body.date)
+      .where('member_id', '==', req.body.member_id)
+      .get()
+    if (0 < attendance.docs.length) {
+      res.status(404).end()
+    } else {
+      const { id } = await db.collection('attendance').add({
+        ...req.body,
+      })
+      res.status(200).json({ id })
+    }
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
