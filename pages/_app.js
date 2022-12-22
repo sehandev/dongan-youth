11914ca1039ef16f7a0a6fac66c86a248d2926e3
sehandev <sehandev@gmail.com>
2022-12-22 11:49:26 +0000

    Add swr config

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
+import { SWRConfig } from 'swr'
 import '../styles/globals.css'
 import wrapper from '../store'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig
+      value={{
+        shouldRetryOnError: false,
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
 }
 
 export default wrapper.withRedux(MyApp)
